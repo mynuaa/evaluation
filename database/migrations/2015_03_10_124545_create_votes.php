f179cb29208d8012aa2114cdd724039868afe76c
Usual <cuihao5135@gmail.com>
2015-03-10 20:46:22 +0800

    Create table votes.

---
+++ database/migrations/2015_03_10_124545_create_votes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Votes extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('votes', function($table){
+			$table->increments('id');
+
+			$table->integer('from_id');
+			$table->integer('to_id');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('votes');
+	}
+
+}
