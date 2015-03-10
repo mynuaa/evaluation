07d3c8508fa66c7bc61d4557e60b4b999c395752
Usual <cuihao5135@gmail.com>
2015-03-10 20:17:24 +0800

    Create table recommendations

---
+++ database/migrations/2015_03_10_121416_create_recommendations.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecommendations extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('recommendations', function($table){
+			$table->increments('id');
+			$table->integer('from_id');
+			$table->integer('to_id');
+			$table->text('content');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('recommendations');
+	}
+
+}
