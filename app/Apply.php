fa52b9ea9108fa592595e21b9ae6e0d9e9970836
Usual <cuihao5135@gmail.com>
2015-03-11 10:48:38 +0800

    Create model Apply half.

---
+++ app/Apply.php
@@ -0,0 +1,19 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes as SoftDeletes;
+
+class Apply extends Model {
+
+	use SoftDeletes;
+
+	protected $fillable = ['user_id', 'type', 'opition', 'whoami', 'story'];
+
+	protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+	public function user()
+	{
+		return $this->belongsTo('User');
+	}
+
+}
