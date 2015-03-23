56aeb183798cbaea66d49958dbb436ea1d756f2f
Usual <cuihao5135@gmail.com>
2015-03-23 22:00:27 +0800

    Repaire language

--- resources/lang/zh/message.php
+++ resources/lang/zh/message.php
@@ -3,7 +3,7 @@
 return [
 	'login_needed' => '请先登录！',
 	'login_successed' => '登录成功！',
-	'login_failed' => '登录失败'
+	'login_failed' => '登录失败',
 	'apply_successed' => '申报成功！',
-	'update_successed' => '资料更新成功！'
+	'update_successed' => '资料更新成功'
 ];
