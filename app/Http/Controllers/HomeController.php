fa61ae957b3fe065cdfc3235e1f3549c38019b2b
Usual <cuihao5135@gmail.com>
2015-03-23 20:25:07 +0800

    Fixed root path.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,21 +14,11 @@ class HomeController extends Controller {
 	*/
 
 	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
-
-	/**
 	 * Show the application dashboard to the user.
 	 *
 	 * @return Response
 	 */
-	public function getIndex()
+	public function index()
 	{
 		return view('home');
 	}
