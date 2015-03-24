1e8f42f3722aab7c24d9b181bd54f6925ae25059
Usual <cuihao5135@gmail.com>
2015-03-24 20:17:44 +0800

    Temporary commit.

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Apply, App\Http\Requests\DetailsGetRequest;
+
+class SearchController extends Controller {
+
+	public function getSchool()
+	{
+		$applies = Apply::type(config('business.type.school'))->get();
+
+		return view('search.view')->withApplies($applies);
+	}
+
+	public function getCollege($cid)
+	{
+		$applies = Apply::type(config('business.type.school'))->college($cid)->get();
+
+		return view('search.view')->withApplies($applies);		
+	}
+
+	public function getDetails(DetailsGetRequest $request)
+	{
+		
+	}
+}
--- config/business.php
+++ config/business.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-	
+	'type' => [
+		0 => 'school',
+		1 => 'college',
+		'school' => 0,
+		'college' => 1
+	]
 ];
