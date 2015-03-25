ea12b1fabe49529ec6b9ab9f0eb9673a3e547573
Rex Zeng <rex.fgtsky@qq.com>
2015-03-25 18:22:15 +0800

    .

--- resources/views/apply/show.blade.php
+++ resources/views/apply/show.blade.php
@@ -11,21 +11,19 @@
 		<div>{{ $apply->major }}专业</div>
 	</div>
 </div>
-<div>
-	<p class="tip">浏览：{{ $apply->pageview }}次</p>
-</div>
 <h5>我是这样一个人</h5>
-<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', $apply->whoami . "\n") !!}</p>
+<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', htmlspecialchars($apply->whoami) . "\n") !!}</p>
 <h5>我的故事</h5>
-<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', $apply->story . "\n") !!}</p>
+<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', htmlspecialchars($apply->story) . "\n") !!}</p>
 <h5>我的不足</h5>
-<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', $apply->insufficient . "\n") !!}</p>
+<p class="indent">{!! preg_replace('/(.+)[\r\n]/', '<p class="indent">$1</p>', htmlspecialchars($apply->insufficient) . "\n") !!}</p>
 <div class="rs-form fullwidth">
 	<div class="rs-form-btns">
-		<input type="button" value="返回" class="btn-primary" onclick="window.history.go(-1)">
 		<input type='button' value="投票" class="btn-success" onclick="window.location.href='{{ url('apply/vote/'.$apply->id) }}'">
+		<p class="tip">浏览：{{ $apply->pageview }}次</p>
 	</div>
 </div>
+<hr>
 <h3>我要推荐</h3>
 <form action='{{ url("apply/recommendation") }}' method="post" class="rs-form fullwidth">
 	<input name='applyid' type='hidden' value="{{ $apply->id }}">
