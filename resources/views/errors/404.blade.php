68ec6765b9b556f84fd99309648690f083c9ac35
Rex Zeng <rex.fgtsky@qq.com>
2015-04-01 03:35:19 +0800

    .

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,20 +5,19 @@
 @section('content')
 <style>
 	#contain{
-		margin: 100px auto;
+		margin: 20px auto;
 		max-width:350px;
 		height: 300px;
 		background: #E74C3C;
 		border-radius: 5px;
 		color: #FFF;
-
 	}
 	#header{
 		background: #2ECC71;
-		border-radius: 5px;
 		padding: 10px 0;
-		text-indent: 10px;
-		
+		text-align: center;
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
 	}
 	#head{
 		font-size: 28px;
@@ -27,7 +26,6 @@
 	}
 	#content{
 		line-height: 20px;
-		/*letter-spacing: 5px;*/
 		padding-left: 30px;
 		margin-top: 30px;
 	}
@@ -36,16 +34,11 @@
 	}
 </style>
 <div id="contain">
-	<div id="header">
-		404_Not_Found
-	</div>
-	<div id="head">
-		咦！？好像出错了！
-	</div>
+	<div id="header">404_Not_Found</div>
+	<div id="head">你似乎访问了</div>
+	<div id="head">一个<b>不存在</b>的页面</div>
 	<div id="content">
-		<li>你可以尝试<b>刷新</b>页面</li>
-		<li>返回上一级</li>
-		<li>检查网络状况</li>
+		<li class="pointer" onclick="window.history.go(-1)">返回上一级</li>
 	</div>
 </div>
 @stop
