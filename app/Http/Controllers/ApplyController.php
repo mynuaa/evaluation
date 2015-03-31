213a6f5aaf741d8a5987cb32da848be2def87feb
Usual <532000663@qq.com>
2015-04-01 03:54:36 +0800

    Close vote.

--- app/Http/Controllers/ApplyController.php
+++ app/Http/Controllers/ApplyController.php
@@ -113,6 +113,7 @@ class ApplyController extends Controller {
 
 	public function getVote($id)
 	{
+		return redirect()->back()->withMessage(['type' => 'error', 'content' => '投票还没开始啊喂~']);
 
 		if (Auth::user()->isVoted($id))
 		{
