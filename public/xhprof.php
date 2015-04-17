55e35c4f6f3103cced8dafd7673679dc6d76ebad
Usual <cuihao5135@gmail.com>
2015-04-17 09:30:40 +0800

    .

--- public/xhprof.php
+++ public/xhprof.php
@@ -1,10 +1,18 @@
 <?php
 
-	xhprof_enable();
+	xhprof_enable(XHPROF_FLAGS_CPU+XHPROF_FLAGS_MEMORY);
 
 	echo 1;
 
 	$data = xhprof_disable();
+
+	$xhprof_root = "/usr/share/xhprof";
+
+	include_once $xhprof_root."/xhprof_lib/utils/xhprof_lib.php";
+	include_once $xhprof_root."/xhprof_lib/utils/xhprof_runs.php";
+
+	$xhprof_runs = new XHprofRuns_Default();
+	$run_id = $xhprof_runs->save_run($data, "test");
 
 	var_dump($data);
 ?>
