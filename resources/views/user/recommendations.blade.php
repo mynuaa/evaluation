e50b267d7af5a04aa94fd73a25b7dcf7b1e4077c
Rex Zeng <rex.fgtsky@qq.com>
2016-04-06 17:07:31 +0800

    修改了推荐和投票记录的位置

--- resources/views/user/recommendations.blade.php
+++ resources/views/user/recommendations.blade.php
@@ -3,8 +3,25 @@
 @section('title'){{ trans('recommend.title') }}@stop
 
 @section('content')
+<h3>我的推荐</h3>
+@if ($recommendations->count() != 0)
+	@foreach ($recommendations as $rec)
+	<div class="card-outer" onclick="location.href='{{ url('apply/show/' . $rec->apply_id) . '#apply_a_' . $rec->id }}'">
+		<div class="card-inner">
+			<h5 class="card-content">
+				{{ trans('recommend.where', ['title' => $rec->apply->title, 'name' => $rec->apply->name]) }}
+			</h5>
+			<blockquote class="card-content card-describtion">{{ $rec->content }}</blockquote>
+		</div>
+	</div>
+	@endforeach
+@else
+	<div class="rs-message">
+		<div class="rs-msg rs-msg-info">{{ trans('recommend.none') }}</div>
+	</div>
+@endif
+<hr>
 <h3>我的投票</h3>
-
 <div class="rs-message">
 	<div class="rs-msg rs-msg-info">
 		你已投同学院{{ $details['inner'] }}票，不同学院{{ $details['outer'] }}票。
@@ -13,7 +30,6 @@
 	按照规则，总票数一共{{ config('business.vote.max') }}票，其中同学院必须最少1票，最多{{ config('business.vote.inner') - $details['inner'] }}票，不同学院不能少于{{ intval($details['inner']) / 2 }}票。
 	</div>
 </div>
-
 @if ($votes->count() != 0)
 	@foreach ($votes as $rec)
 	<a href="{{ url('apply/show/' . $rec->id) }}">
@@ -31,24 +47,6 @@
 @else
 	<div class="rs-message">
 		<div class="rs-msg rs-msg-warning">{{ trans('vote.none') }}</div>
-	</div>
-@endif
-<hr>
-<h3>我的推荐</h3>
-@if ($recommendations->count() != 0)
-	@foreach ($recommendations as $rec)
-	<div class="card-outer" onclick="location.href='{{ url('apply/show/' . $rec->apply_id) . '#apply_a_' . $rec->id }}'">
-		<div class="card-inner">
-			<h5 class="card-content">
-				{{ trans('recommend.where', ['title' => $rec->apply->title, 'name' => $rec->apply->name]) }}
-			</h5>
-			<blockquote class="card-content card-describtion">{{ $rec->content }}</blockquote>
-		</div>
-	</div>
-	@endforeach
-@else
-	<div class="rs-message">
-		<div class="rs-msg rs-msg-info">{{ trans('recommend.none') }}</div>
 	</div>
 @endif
 @stop
