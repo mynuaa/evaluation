21f33c12b2d808483e77aa6c7a4ea8e0b839cb01
Rex Zeng <rex.fgtsky@qq.com>
2016-04-08 13:44:59 +0800

    修复投票的旧数据问题

--- app/Vote.php
+++ app/Vote.php
@@ -5,6 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 class Vote extends Model {
 
 	public function apply(){
-		return $this->belongsTo('App\Apply');
+		return $this->belongsTo('App\Apply')->whereNull('votes.deleted_at');
 	}
 }
