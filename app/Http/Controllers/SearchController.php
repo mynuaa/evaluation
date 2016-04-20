ad6533057836a940daad2cb4baa373e18fb340ed
Rex Zeng <rex.fgtsky@qq.com>
2016-04-20 15:38:58 +0800

    按照随机序排序

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,28 +11,28 @@ class SearchController extends Controller {
 
 	public function index()
 	{
-		$applies = Apply::where('old', false)->order()->paginate(config('business.paginate'));
+		$applies = Apply::where('old', false)->orderByRaw("RAND()")->paginate(config('business.paginate'));
 
 		return view('search.view')->withApplies($applies)->withStatistics($this->statistics());
 	}
 
 	public function getOld()
 	{
-		$applies = Apply::where('old', true)->order()->paginate(config('business.paginate'));
+		$applies = Apply::where('old', true)->orderByRaw("RAND()")->paginate(config('business.paginate'));
 
 		return view('search.view')->withApplies($applies)->withStatistics($this->statistics());
 	}
 
 	public function getSchool()
 	{
-		$applies = Apply::type(config('business.type.school'))->where('old', false)->order()->paginate(config('business.paginate'));
+		$applies = Apply::type(config('business.type.school'))->where('old', false)->orderByRaw("RAND()")->paginate(config('business.paginate'));
 
 		return view('search.view')->withApplies($applies)->withStatistics($this->statistics());
 	}
 
 	public function getCollege($cid)
 	{
-		$applies = Apply::type(config('business.type.college'))->where('old', false)->college($cid)->order()->paginate(config('business.paginate'));
+		$applies = Apply::type(config('business.type.college'))->where('old', false)->college($cid)->orderByRaw("RAND()")->paginate(config('business.paginate'));
 
 		return view('search.view')->withApplies($applies)->withStatistics($this->statistics());
 	}
@@ -42,7 +42,7 @@ class SearchController extends Controller {
 		$type = $request->type;
 		$key = $request->key;
 
-		$applies = Apply::where('old', false)->where($type, $key)->order()->paginate(config('business.paginate'));
+		$applies = Apply::where('old', false)->where($type, $key)->orderByRaw("RAND()")->paginate(config('business.paginate'));
 
 		return view('search.view')->withApplies($applies)->withStatistics($this->statistics());
 	}
