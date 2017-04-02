3d2da2ee2daf9c9f464ada86d952c1f7100d0986
wiwry <laigq2015@163.com>
2017-04-02 12:12:41 +0800

    修复管理员页面姓名不匹配

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -35,8 +35,8 @@ class AdminController extends Controller {
 	}
 	public function getShowonerecommendations($apply_id=1)
 	{
-		$records = DB::table('recommendations')->join('applies', 'applies.id', '=', 'apply_id')->where('apply_id',$apply_id)->get();
-		$Owner = DB::table('users')->select('name')->where('id',$apply_id)->get();
+		$records = DB::table('recommendations')->join('users', 'users.id', '=', 'user_id')->where('apply_id',$apply_id)->get();
+		$Owner = DB::table('applies')->select('name')->where('id',$apply_id)->get();
 		return view('admin/showonerecommendations')->withRecords($records)->withOwner($Owner[0]);
 	}
 
