6847db776fabe8b793c7b2c9f8f66aea8516d2e6
wiwry <laigq2015@163.com>
2017-04-08 23:51:45 +0800

    添加登入出错反馈

--- resources/lang/zh/login.php
+++ resources/lang/zh/login.php
@@ -9,7 +9,7 @@ return [
 		'若是登入遇到问题，请发送邮件到zfj@nuaa.edu.cn与技术人员取得联系',
 	],
 	'success' => '登录成功！',
-	'failed' => '登录失败！',
+	'failed' => '登录失败！请发送邮件到zfj@nuaa.edu.cn与技术人员取得联系',
 	'username' => '用户名',
 	'password' => '密码',
 	'login' => '登录',
