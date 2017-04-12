51b60db79b9dc0b7ca572482289785056b5afc70
Seiry <yuwenjie66@126.com>
2017-04-12 09:50:15 +0800

    加一个

--- app/User.php
+++ app/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	}
 
 	public function votes() {
-		return $this->belongsToMany('App\Apply', 'votes')->whereNull('votes.deleted_at')->withTimestamps();
+		return $this->belongsToMany('App\Apply', 'votes')->where('year',2017)->whereNull('votes.deleted_at')->withTimestamps();
 	}
 
 	public function myRecommendations() {
