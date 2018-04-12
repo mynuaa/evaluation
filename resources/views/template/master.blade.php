aa487b018a71bf00f212ef8358f0fa7ec6bef101
Seiry <yuwenjie66@126.com>
2018-04-12 13:23:34 +0800

    增加renderer

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -25,6 +25,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
+	<meta name="renderer" content="webkit">
 	<link rel="stylesheet" href="{{ asset('/css/rscss.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/font-awesome.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/main.css') }}">
