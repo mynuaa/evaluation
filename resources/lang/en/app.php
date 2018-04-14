73dec571899818b56b8b6e20c27a1ceb8e9ea076
slx <zhjhzhshlx@sina.com>
2018-04-14 11:29:12 +0800

    ‘“修改截止时间通知

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -15,7 +15,8 @@ return [
 		'update' => 'Update',
 		'logout' => 'Log out',
 		'detail' => 'Detail',
-		'warning' => 'Please ensure that the login information is exactly the same as the Academic Office or Graduate School website. If there is a phenomenon that the number of votes is cast to a stranger, please contact the Youth League Committee in time and provide screenshots.'
+		'warning' => 'Please ensure that the login information is exactly the same as the Academic Office or Graduate School website. If there is a phenomenon that the number of votes is cast to a stranger, please contact the Youth League Committee in time and provide screenshots.',
+		'notes' => 'In order to better meet the reporting requirements of the students, the deadline for personal application and teacher/student recommendation is postponed to 23:59 on April 15th.'
 	],
 	'ie' => 'Please use modern browser like Chrome, Firefox, IE(≥9), etc. to access this page.',
 	'download' => 'Click here',
@@ -28,10 +29,10 @@ return [
 	'mail' => 'If you have any questions, please mail to ',
 	'notice' => [
 		'1. Before activity, please read <b><a href="/evaluation/notes-2018.docx">Notes (Click to download)</a></b> first.',
-		'2. Timeline: Apply &amp; recommendation during 4.9-4.14; college verify during 4.9-4.16; vote during 4.17-4.19.',
-		'3. Consultation and complaint please mail to <a href="mailto:nuaatw@126.com">nuaatw@163.com/a>.',
+		'2. Timeline: Apply &amp; recommendation (The deadline is postponed until 23:59 on April 15); college verify during 4.9-4.16; vote during 4.17-4.19.',
+		'3. Consultation and complaint please mail to <a href="mailto:nuaatw@126.com">nuaatw@163.com</a>.',
 		/*'4. Up to now, we have 805 applies, 12976 recommendations. You can vote for 660 applies which have not less than 10 recommendations.',*/
-		'4. If an error occurs during the operation, please send a screenshot of the error message to <a href="mailto:zfj@nuaa.edu.cn">zfj@nuaa.edu.cn/a> so that the technician can locate the error and resolve it in time.',
+		'4. If an error occurs during the operation, please send a screenshot of the error message to <a href="mailto:zfj@nuaa.edu.cn">zfj@nuaa.edu.cn</a> so that the technician can locate the error and resolve it in time.',
 	],
 	// 'statistics' => '5. Up to now, we have :apply application(s), :recommendation recommendation(s).',
 	'statistics' => '',
