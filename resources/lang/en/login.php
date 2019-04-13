9e8edb0aa58b44c120cfe9b4ef233062c53ed884
Seiry Yu <yuwenjie66@126.com>
2019-04-13 18:43:24 +0800

    更改登录文案

--- resources/lang/en/login.php
+++ resources/lang/en/login.php
@@ -4,7 +4,8 @@ return [
 	'title' => 'Log In',
 	'intro' => [
 		'Undergraduate students please use the same account name and password as ded account to log in.',
-		'Postgraduate students please use the same account name and password as graduate school account to log in.',
+		// 'Postgraduate students please use the same account name and password as graduate school account to log in.',
+		'Postgraduate students please use the same account name and password as c.a.s to log in.',
 		'Teachers please use the same account name and password as personnel department website to log in.',
 	],
 	'success' => 'Login successed!',
